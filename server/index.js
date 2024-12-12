6411b33afaac4521bacd2501b29e76967bc195dc
Harshvardhan <shreeharshpb11@gmail.com>
2024-12-12 10:43:46 +0530

    configure index.js file

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT;
+const databaseURL = process.env.MONGODB_URL;
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser())
+
+const server = app.listen(port, () => {
+    console.log(`server is running on ${port}`)
+})
+
+mongoose
+    .connect(databaseURL)
+    .then(() => {
+        console.log('MongoDB Connected Successfuly')
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
